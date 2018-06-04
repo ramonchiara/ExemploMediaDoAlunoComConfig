7ec2a081f1159fe73ab6f67fb3701bc8fbe0c6b1
Ramon Chiara <ramonchiara@gmail.com>
2018-06-04 00:47:25 -0300

    Programa mostrando o problema.

---
+++ ExemploMediaDoAlunoComConfig/Aluno.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ExemploMediaDoAlunoComConfig
+{
+	public class Aluno
+	{
+		private string nome;
+		private double p1, p2;
+		private double mediaMinima = 6;
+
+		public Aluno(string nome, double p1, double p2)
+		{
+			this.nome = nome;
+			this.p1 = p1;
+			this.p2 = p2;
+		}
+
+		public string GetNome()
+		{
+			return nome;
+		}
+
+		public void SetMediaMinima(double mediaMinima)
+		{
+			this.mediaMinima = mediaMinima;
+		}
+
+		public double GetMedia()
+		{
+			return (p1 + p2) / 2;
+		}
+
+		public bool Passou {
+			get {
+				return GetMedia() >= mediaMinima;
+			}
+		}
+	}
+}
---
+++ ExemploMediaDoAlunoComConfig/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExemploMediaDoAlunoComConfig
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Aluno aluno1 = new Aluno("Alice", 7, 7);
+			Aluno aluno2 = new Aluno("Bruno", 6, 6);
+			Aluno aluno3 = new Aluno("Clara", 5, 5);
+
+			aluno1.SetMediaMinima(5);
+
+			Console.WriteLine("{0} {1}", aluno1.GetNome(), aluno1.Passou ? "passou" : "não passou");
+			Console.WriteLine("{0} {1}", aluno2.GetNome(), aluno2.Passou ? "passou" : "não passou");
+			Console.WriteLine("{0} {1}", aluno3.GetNome(), aluno3.Passou ? "passou" : "não passou");
+		}
+	}
+}
